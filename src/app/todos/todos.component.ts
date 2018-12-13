ea93c4e7de21dbc624e7d91f964c775151147dd6
Safandhi Yonathan <safandhi.yonathan@gmail.com>
2018-12-13 13:44:15 +0700

    new

---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,38 @@
+import { Component} from '@angular/core';
+import { trigger, animate , state, transition, style } from '@angular/animations';
+
+@Component({
+  selector: 'todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css'],
+  animations: [
+    trigger('fade', [
+
+      state('void', style({ opacity: 0 })),
+      
+      transition(
+        // 'void => *, * => void', 
+        // 'void <=> *',
+        ':enter, :leave',
+        [
+        animate(1000)
+      ])
+    ])
+  ]
+})
+export class TodosComponent {
+  items: any[] = [
+    'Wash the dishes', 
+    'Call the accountant', 
+    'Apply for a car insurance'];
+
+  addItem(input: HTMLInputElement) {
+    this.items.splice(0, 0, input.value);
+    input.value = ''; 
+  }
+
+  removeItem(item) {
+    let index = this.items.indexOf(item);
+    this.items.splice(index, 1);
+  }
+}
