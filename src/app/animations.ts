4a12b1dce9ddbbd962f343be8177d3d78efef164
Safandhi Yonathan <safandhi.yonathan@gmail.com>
2018-12-13 14:14:13 +0700

    slide

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,14 +1,24 @@
 import { trigger, animate , state, transition, style } from '@angular/animations';
 
+export let slide = trigger('slide', [
+  transition(':enter', [
+    style({ transform: 'translateX(-10px)'}),
+    animate(500)
+  ]),
+  transition(':leave', [
+    animate(500, style({ transform: 'translateX(-100px)'}))
+  ])
+]);
+
 export let fade =   trigger('fade', [
 
-  state('void', style({ opacity: 0 })),
+  state('void', style({ transform: 'translateX(-20px)', opacity: 0 })),
   
   transition(
     // 'void => *, * => void', 
     // 'void <=> *',
     ':enter, :leave',
     [
-    animate(1000)
+    animate(500)
   ])
-])
+]);
